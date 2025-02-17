90a8a7e6e6d60e375d4ba9b86dbc41f014e70b88
Mohamed Asad <mohamedasad11914@gmail.com>
2025-02-17 23:06:43 +0200

    Modules added

--- index.js
+++ index.js
@@ -9,19 +9,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Set up EJS for rendering views
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const app = require("./app");
+const personRoutes = require("./routes/personRoutes");
+app.use("/api/person", personRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-const personRoutes = require("./routes/personRoutes");
-app.use("/api/person", personRoutes);
